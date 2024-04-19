e885b493f5112b5ca237fe3d79e0f90fa2608bf0
JaeJun <yyggb12@kakao.com>
2024-04-19 17:25:03 +0900

    Initial commit

---
+++ src/main/java/jyang/deliverydotdot/DeliveryDotDotApplication.java
@@ -0,0 +1,13 @@
+package jyang.deliverydotdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDotDotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryDotDotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jyang/deliverydotdot/DeliveryDotDotApplicationTests.java
@@ -0,0 +1,13 @@
+package jyang.deliverydotdot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDotDotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
